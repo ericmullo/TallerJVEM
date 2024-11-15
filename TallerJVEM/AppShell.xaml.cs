d6034df2ef8e165aeb64b6220210885276fcd26d
laboratorio <laboratorio@UPOAULA10621>
2024-11-15 08:01:40 -0500

    Agregar archivos de proyecto.

---
+++ TallerJVEM/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TallerJVEM
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TallerJVEM/paginaprincipal.xaml.cs
@@ -0,0 +1,15 @@
+namespace TallerJVEM;
+
+public partial class paginaprincipal : ContentPage
+{
+	public paginaprincipal()
+	{
+		InitializeComponent();
+	}
+
+    private void Boton2_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+
+    }
+}
